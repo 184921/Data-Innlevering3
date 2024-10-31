52be8136384f0fd015645067e9378ef22fd059f9
184921 <Ronnykicho@gmail.com>
2024-10-31 16:21:56 +0100

    n

--- src/main/java/no/hvl/dat100/oppgave1/Innlegg.java
+++ src/main/java/no/hvl/dat100/oppgave1/Innlegg.java
@@ -2,70 +2,93 @@ package no.hvl.dat100.oppgave1;
 
 import no.hvl.dat100.common.TODO;
 
-public abstract class Innlegg {
+public abstract class Innlegg 
+{
+	private int id;
+	private String bruker;
+	private String dato;
+	private int likes;
 	
 	// TODO - deklarering av objektvariable
 	
-	public Innlegg() {
+	public Innlegg() 
+	{
 		
 	}
 	
-	public Innlegg(int id, String bruker, String dato) {
-
-		// TODO 
-		throw new UnsupportedOperationException(TODO.constructor("Innlegg"));
+	public Innlegg(int id, String bruker, String dato) 
+	{
+		this.likes = 0;
+		this.id = id;
+		this.bruker = bruker;
+		this.dato = dato;
 	}
 
-	public Innlegg(int id, String bruker, String dato, int likes) {
-
-		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.constructor("Innlegg"));
-	}
-	
-	public String getBruker() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-
-	}
-
-	public void setBruker(String bruker) {
-		throw new UnsupportedOperationException(TODO.method());
-	}
-
-	public String getDato() {
-		throw new UnsupportedOperationException(TODO.method());
-		
-	}
-
-	public void setDato(String dato) {
-		throw new UnsupportedOperationException(TODO.method());
-	}
-
-	public int getId() {
-		throw new UnsupportedOperationException(TODO.method());
-
-	}
-
-	public int getLikes() {
-		throw new UnsupportedOperationException(TODO.method());
-
+	public Innlegg(int id, String bruker, String dato, int likes) 
+	{
+		this.likes = likes;
+		this.id = id;
+		this.bruker = bruker;
+		this.dato = dato;
 	}
 	
-	public void doLike () {
-		throw new UnsupportedOperationException(TODO.method());
+	public String getBruker() 
+	{
+		return bruker;
+	}
+
+	public void setBruker(String bruker) 
+	{
+		this.bruker = bruker;
+	}
+
+	public String getDato() 
+	{
+		return dato;
+	}
+
+	public void setDato(String dato) 
+	{
+		this.dato = dato;
+	}
+
+	public int getId() 
+	{
+		return id;
 	}
 	
-	public boolean erLik(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
+	public void setId(int id) 
+	{
+		this.id = id;
+	}
 
+	public int getLikes() 
+	{
+		return likes;
+	}
+	
+	public void doLike () 
+	{
+		this.likes += 1;
+	}
+	
+	public boolean erLik(Innlegg innlegg) 
+	{
+		boolean lik = false;
+		
+		if(innlegg.getId() == id) 
+		{
+			lik = true;
+		}
+		
+		return lik;
 	}
 	
 	@Override
-	public String toString() {
-		
-		throw new UnsupportedOperationException(TODO.method());
-				
+	public String toString() 
+	{
+		String bruker = this.bruker;
+		return id + "\n" + bruker + "\n" + dato + "\n" + likes + "\n";
 	}
 	
 	// Metoden nedenfor er kun for valgfri oppgave 6
--- src/main/java/no/hvl/dat100/oppgave2/Tekst.java
+++ src/main/java/no/hvl/dat100/oppgave2/Tekst.java
@@ -3,35 +3,44 @@ package no.hvl.dat100.oppgave2;
 import no.hvl.dat100.common.TODO;
 import no.hvl.dat100.oppgave1.*;
 
-public class Tekst extends Innlegg {
+public class Tekst extends Innlegg 
+{
+	String tekst;
+	
 
 	// TODO: objektvariable 
 	
-	public Tekst () {
+	public Tekst () 
+	{
 		
 	}
 	
-	public Tekst(int id, String bruker, String dato, String tekst) {
-		throw new UnsupportedOperationException(TODO.constructor("Tekst"));
+	public Tekst(int id, String bruker, String dato, String tekst) 
+	{
+		super(id, bruker, dato);
+		this.tekst = tekst;
 	}
 	
-	public Tekst(int id, String bruker, String dato, int likes, String tekst) {
-		throw new UnsupportedOperationException(TODO.constructor("Tekst"));
+	public Tekst(int id, String bruker, String dato, int likes, String tekst)
+	{
+		super(id, bruker, dato, likes);
+		this.tekst = tekst;
 	}
 	
-	public String getTekst() {
-		throw new UnsupportedOperationException(TODO.method());
-
+	public String getTekst() 
+	{
+		return tekst;
 	}
 
-	public void setTekst(String tekst) {
-		throw new UnsupportedOperationException(TODO.method());
+	public void setTekst(String tekst) 
+	{
+		this.tekst = tekst;
 	}
 
 	@Override
-	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
-
+	public String toString() 
+	{
+		return "TEKST\n" + getId() + "\n" + getBruker() + "\n" + getDato() + "\n" + getLikes() + "\n" + tekst + "\n";
 	}
 	
 	// Metoden nedenfor er kun for valgfri oppgave 6
